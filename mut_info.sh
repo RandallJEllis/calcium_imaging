7f09d82cabfcbc8e43747ba7c3837f2144704afb
Randy Ellis <randall.ellis@tufts.edu>
2023-05-25 11:39:55 -0400

    added mut_info.sh

--- mut_info.sh
+++ mut_info.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 #SBATCH -J mut_info
 #SBATCH --time=04-00:00:00
-#SBATCH -p batch
+#SBATCH -p largemem
 #SBATCH -N 1 #nodes
-#SBATCH -n 2 #tasks (can stand in for cores)
-#SBATCH --mem=8gb #Memory requested
-#SBATCH --output=create_bigtiff.%j.%N.out
-#SBATCH --error=create_bigtiff.%j.%N.err
+#SBATCH -n 10 #tasks (can stand in for cores)
+#SBATCH --mem=64gb #Memory requested
+#SBATCH --output=sh_mut_info.%j.%N.out
+#SBATCH --error=sh_mut_info.%j.%N.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=randall.ellis@tufts.edu
 
 module load anaconda/2021.11
 source activate suite2p
-python mut_info.py $1
+python mut_info.py "$@"
