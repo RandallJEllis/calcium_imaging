c8f6f5838b70980aa3afe031e44fd5599ffdfa5b
Randy Ellis <randall.ellis@tufts.edu>
2023-05-25 11:31:15 -0400

    revised mut_info.py

--- mut_info.py
+++ mut_info.py
@@ -8,34 +8,37 @@ from varley.local_mi import local_total_correlation
 from tqdm import tqdm
 
 '''
-Calculate mutual information between all pairs of vectors in a directory tree of cell spike data 
-and store the resulting mutual information matrix.
-
-Args:
-    root (str): the path to the root directory of the cell spike data.
-
-The script searches through all subdirectories in the given root directory for subdirectories 
-containing the string '5min'. For each of these subdirectories, it loads the cell spike data from
-the file 'cell_spks.npy' in the subdirectory 'bigtiffs/suite2p/plane0', calculates the mutual 
-information between all pairs of vectors using mutual_info_regression() from the scikit-learn 
-package, and saves the resulting mutual information matrix as a NumPy binary file 
-'mut_info_matrix.npy' in the same subdirectory.
-
-Note that the mutual information values are only calculated for a subset of the time series data 
-(from time steps 1000 to 6000), and that the script skips over subdirectories for which the mutual
-information matrix file already exists.
-
+Mutual information calculation for calcium imaging data. 
+The mutual information between two random variables is a measure of the mutual dependence between the two variables.
+The mutual information between two vectors is calculated using the mutual_info_regression function from sklearn.
+The mutual information between two vectors is calculated using the local_total_correlation function from varley.
+Arguments:
+    root (str): Path to the Suite2P output directory.
+    mi_type (str): Type of mutual information calculation, 'global' or 'local'.
+    overwrite (str): Whether to overwrite existing files, 'True' or 'False'.
 Returns:
-    None.
+    None. Saves the mutual information matrix to a file named "mut_info_matrix.npy".
 '''
 
-def mi_parallel(data):
+
+def global_mi(data):
+
+    """
+    Calculate mutual information between pairs of vectors using parallel processing.
+
+    Args:
+        data (ndarray): Input data, a 2D array where each row represents a vector.
+
+    Returns:
+        None. Saves the mutual information matrix to a file named "mut_info_matrix.npy".
+    """
+
     # create a square matrix to store the mutual information values
     n = len(data)
     mi_matrix = np.zeros((n, n))
 
     # calculate the mutual information between all pairs of vectors and store in the matrix
-    with multiprocessing.Pool() as pool:
+    with mp.Pool() as pool:
         futures = []
         for i in range(n):
             for j in range(i+1, n):
@@ -54,6 +57,17 @@ def mi_parallel(data):
 
 
 def local_mi(data):
+
+    """
+    Calculate local mutual information between pairs of vectors using parallel processing.
+
+    Args:
+        data (ndarray): Input data, a 2D array where each row represents a vector.
+
+    Returns:
+        None. Saves the local mutual information matrices to separate files in chunks.
+    """
+
     n = len(data)
     mi_matrix = np.zeros((n, n, data.shape[1]))
 
@@ -86,49 +100,51 @@ def local_mi(data):
             filename = os.path.join(dir, 'bigtiffs', 'suite2p', 'plane0', f'local_mut_info_matrix_chunk_{start}_{end}.npy')
             np.save(filename, mi_matrix[start:end])
 
+if __name__ == '__main__':
+    # Define the path to the Suite2P output
+    root = sys.argv[1]
+    print(root)
 
-# Define the path to the Suite2P output
-root = sys.argv[1]
-print(root)
+    # type of mutual information calculation
+    mi_type = sys.argv[2] # 'local' or 'global'
+    print(mi_type)
 
-mi_type = sys.argv[2] # 'local' or 'global'
-print(mi_type)
+    # whether to overwrite existing files
+    overwrite = sys.argv[3] # 'True' or 'False' for overwriting existing files
 
-overwrite = sys.argv[3] # 'True' or 'False' for overwriting existing files
+    # create an empty list to store the subdirectories
+    subdirectories = []
 
-# create an empty list to store the subdirectories
-subdirectories = []
+    # traverse directories and subdirectories using os.walk()
+    for dirpath, dirnames, filenames in os.walk(root):
+        # append the subdirectories in the current directory to the list if they contain '5min'
+        for dirname in dirnames:
+            if '5min' in dirname:
+                subdirectories.append(os.path.join(dirpath, dirname))
 
-# traverse directories and subdirectories using os.walk()
-for dirpath, dirnames, filenames in os.walk(root):
-    # append the subdirectories in the current directory to the list if they contain '5min'
-    for dirname in dirnames:
-        if '5min' in dirname:
-            subdirectories.append(os.path.join(dirpath, dirname))
-
-# iterate over the subdirectories and save a subsetted spks array for the cell ROIs
-for dir in subdirectories:
-    print(dir)
-    # check if 'cell_spks.npy' exists in the directory
-    if os.path.isfile(os.path.join(dir, 'bigtiffs', 'suite2p', 'plane0', 'cell_spks.npy')):
-        # initialize filename
-        if mi_type == 'global':
-            filename = os.path.join(dir, 'bigtiffs', 'suite2p', 'plane0', 'mut_info_matrix.npy')
-        elif mi_type == 'local':
-            filename = os.path.join(dir, 'bigtiffs', 'suite2p', 'plane0', 'local_mut_info_matrix.npy')
-        if os.path.isfile(filename) and not overwrite:
-            print(f'{filename} already exists')
-        else:
-            print(f'Running {mi_type} mutual information calculation on {dir}\n')
-            print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
-
-            cell_spks = np.load(os.path.join(dir, 'bigtiffs', 'suite2p', 'plane0', 'cell_spks.npy'))
-            cell_spks = cell_spks[:, 1000:6000]
-
+    # iterate over the subdirectories and save a subsetted spks array for the cell ROIs
+    for dir in subdirectories:
+        print(dir)
+        # check if 'cell_spks.npy' exists in the directory
+        if os.path.isfile(os.path.join(dir, 'bigtiffs', 'suite2p', 'plane0', 'cell_spks.npy')):
+            # initialize filename
             if mi_type == 'global':
-                mi_parallel(cell_spks)
+                filename = os.path.join(dir, 'bigtiffs', 'suite2p', 'plane0', 'mut_info_matrix.npy')
             elif mi_type == 'local':
-                local_mi(cell_spks)
+                filename = os.path.join(dir, 'bigtiffs', 'suite2p', 'plane0', 'local_mut_info_matrix.npy')
+            if os.path.isfile(filename) and not overwrite:
+                print(f'{filename} already exists')
+            else:
+                print(f'Running {mi_type} mutual information calculation on {dir}\n')
+                print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
 
-            print(f'{filename} saved')
-            print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+                cell_spks = np.load(os.path.join(dir, 'bigtiffs', 'suite2p', 'plane0', 'cell_spks.npy'))
+                cell_spks = cell_spks[:, 1000:6000]
+
+                if mi_type == 'global':
+                    global_mi(cell_spks)
+                elif mi_type == 'local':
+                    local_mi(cell_spks)
+
+                print(f'{filename} saved')
+                print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))  
